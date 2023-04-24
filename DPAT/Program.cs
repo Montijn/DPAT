619573b5e44edd9ff7237ca234cf02a442ff7f71
Montijn <ms.bennik@student.avans.nl>
2023-04-24 12:42:32 +0200

    First setup

---
+++ DPAT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DPAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
