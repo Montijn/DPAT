1a64fa4cb293eb862e0cd4f8c39cfbf60358d4f4
MitchVerweerden <mverweer@avans.nl>
2023-06-05 08:49:28 +0200

    startup

---
+++ DPAT/Strategies/Parsers/SquareFileStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPAT.Strategies.Parsers
+{
+    internal class SquareFileStrategy : IFileStrategy
+    {
+        public object ParseFile(object data)
+        {
+            String sudokuString = data as String;
+
+            double dimension = Math.Sqrt(sudokuString.Length);
+
+            int[,] sudokuArray = new int[(int)dimension, (int)dimension];
+
+            for(int i = 0; i < dimension; i++)
+            {
+                for(int j = 0; j < dimension; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
