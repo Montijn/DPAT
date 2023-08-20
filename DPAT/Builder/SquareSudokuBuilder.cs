66b66c3bae6df6de9419e0dd6bca0822a8381dd1
Montijn <ms.bennik@student.avans.nl>
2023-08-20 20:40:19 +0200

    add testing

--- DPAT/Builder/SquareSudokuBuilder.cs
+++ DPAT/Builder/SquareSudokuBuilder.cs
@@ -9,17 +9,7 @@ namespace DPAT.Builder
     class SquareSudokuBuilder : SudokuBuilder
     {
         protected SquareSudoku _sudoku;
-        private int[,] initialPuzzle = {
-            { 0, 3, 5, 4, 1, 6, 9, 2, 7 },
-            { 2, 9, 6, 8, 5, 7, 4, 3, 1 },
-            { 4, 1, 7, 2, 9, 3, 0, 5, 8 },
-            { 5, 6, 9, 0, 3, 4, 7, 8, 2 },
-            { 1, 0, 3, 6, 7, 8, 5, 4, 0 },
-            { 7, 4, 8, 5, 2, 9, 1, 6, 3 },
-            { 6, 5, 2, 0, 8, 1, 3, 9, 4 },
-            { 9, 8, 1, 3, 4, 5, 2, 7, 6 },
-            { 3, 7, 4, 9, 6, 2, 8, 1, 5 }
-         };
+        private int[,] initialPuzzle;
         public SquareSudokuBuilder(int [,]initialPuzzle)
         {
             this.initialPuzzle = initialPuzzle;
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b1ab6e4-55ce-4ea2-be42-9823e0db3b7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
